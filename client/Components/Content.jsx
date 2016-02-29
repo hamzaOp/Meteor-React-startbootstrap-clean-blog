4a56ff58d6f73930f550cce66feee605a9b49c22
hamza <meteoroid2016@gmail.com>
2016-02-29 22:42:31 +0000

    loading indicator

--- client/Components/Content.jsx
+++ client/Components/Content.jsx
@@ -1,5 +1,5 @@
 const {
-  CircularProgress
+  RefreshIndicator
 } = mui;
 
 Content = React.createClass({
@@ -8,12 +8,13 @@ Content = React.createClass({
 
       if(this.props.loading){
         let progress = {
-          marginLeft : "350",
-          marginRight : "350",
-          marginTop : "50",
-          marginBottom : "50"
+          marginLeft : "45%",
+          marginRight : "45%",
+          marginTop : "15%",
+          marginBottom : "15%"
         };
-        return (<div><Pager callbacks={this.props.callbacks} counter={this.props.counter} /><CircularProgress size={1} style={progress} /></div>);
+        return (<div><Pager callbacks={this.props.callbacks} counter={this.props.counter} /><RefreshIndicator size={50} left={70} top={0} loadingColor={"#DE4F4F"} status="loading" style={progress}
+    /></div>);
       }
       let posts = this.props.posts.map((post) => {
         return <Post key={post._id} id={post._id} author={post.author} title={post.title} subTitle={post.subTitle} postText={post.postText} />;
