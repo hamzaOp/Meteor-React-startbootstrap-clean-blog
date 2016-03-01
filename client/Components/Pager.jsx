4135001da247d1643502bd66597723b2d53a6884
hamza <meteoroid2016@gmail.com>
2016-03-01 00:13:06 +0000

    Pager

--- client/Components/Pager.jsx
+++ client/Components/Pager.jsx
@@ -1,13 +1,15 @@
+const {
+  RaisedButton
+} = mui;
+
 Pager = React.createClass({
 
   render(){
+    let style = {marginLeft : "10%",marginRight:"10%",marginTop: "5%",marginBottom: "5%"};
     return (<ul className="pager">
-        <li className={this.props.counter == 96 ?  'previous hidden' : 'previous'} onClick={this.props.callbacks.Prev}>
-            <a href=""><strong>&lt;</strong> Older Posts</a>
-        </li>
-        <li className={this.props.counter == 0 ?  'next hidden' : 'next'} onClick={this.props.callbacks.Next}>
-            <a href="">Newer Posts <strong>&gt;</strong></a>
-        </li>
+    <li><RaisedButton disabled={this.props.counter == 96 ?  true : false} onClick={this.props.callbacks.Prev} label="Previous" style={style}  /></li>
+    <li><RaisedButton disabled={this.props.counter == 0 ?   true : false} onClick={this.props.callbacks.Next} label="Next" style={style} /></li>
+
     </ul>);
   }
 })
